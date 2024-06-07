ffce941037a89c3467a15b9ac208d954f5410a6b
MisaDev <56828851+MisaelGuzman@users.noreply.github.com>
2024-06-07 01:06:25 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,50 +1,58 @@
-// create web server
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var url = require('url');
-var comments = [];
-var server = http.createServer(function (req, res) {
-    var parseObj = url.parse(req.url, true);
-    var pathname = parseObj.pathname;
+// Create web server
+// HTTP server
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const qs = require('querystring');
+const template = require('./lib/template.js');
+const path = require('path');
+const sanitizeHtml = require('sanitize-html');
+
+const app = http.createServer(function (request, response) {
+    const _url = request.url;
+    const queryData = url.parse(_url, true).query;
+    const pathname = url.parse(_url, true).pathname;
+
     if (pathname === '/') {
-        fs.readFile('./views/index.html', function (err, data) {
-            if (err) {
-                return res.end('404 Not Found');
-            }
-            res.end(data);
-        });
-    } else if (pathname === '/post') {
-        fs.readFile('./views/post.html', function (err, data) {
-            if (err) {
-                return res.end('404 Not Found');
-            }
-            res.end(data);
-        });
-    } else if (pathname.indexOf('/public/') === 0) {
-        fs.readFile('.' + pathname, function (err, data) {
-            if (err) {
-                return res.end('404 Not Found');
-            }
-            res.end(data);
-        });
-    } else if (pathname === '/comments') {
-        var comment = parseObj.query;
-        comment.dateTime = new Date();
-        comments.push(comment);
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        res.end();
-    } else {
-        fs.readFile('./views/404.html', function (err, data) {
-            if (err) {
-                return res.end('404 Not Found');
-            }
-            res.end(data);
-        });
-    }
-});
-// listen port
-server.listen(3000, function () {
-    console.log('Server is running...');
-});
+        if (queryData.id === undefined) {
+            fs.readdir('./data', function (error, filelist) {
+                const title = 'Welcome';
+                const description = 'Hello, Node.js';
+                const list = template.list(filelist);
+                const html = template.HTML(title, list,
+                    `<h2>${title}</h2>${description}`,
+                    `<a href="/create">create</a>`);
+                response.writeHead(200);
+                response.end(html);
+            });
+        } else {
+            fs.readdir('./data', function (error, filelist) {
+                const filteredId = path.parse(queryData.id).base;
+                fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
+                    const title = queryData.id;
+                    const sanitizedTitle = sanitizeHtml(title);
+                    const sanitizedDescription = sanitizeHtml(description, {
+                        allowedTags: ['h1']
+                    });
+                  
+                    const list = template.list(filelist);
+                    const html = template.HTML(sanitizedTitle, list,
+                        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+                        `<a href="/create">create</a> 
+                        <a href="/update?id=${sanitizedTitle}">update</a>
+                        <form action="delete_process" method="post">
+                            <input type="hidden" name="id" value="${sanitizedTitle}">
+                            <input type="submit" value="delete">
+                        </form>`);
+                    response.writeHead(200);
+                    response.end(html);
+                });
+            });
+        }
+    } else if (pathname === '/create') {
+        fs.readdir('./data', function (error, filelist) {
+            const title = 'WEB - create';
+            const list = template.list(filelist);
+            const html = template.HTML(title, list, `
+                <form action="/create_process" method="post">
+                    <p><input type="
